320b297c639e3b57d2031d89610ef42ad0bb20df
lev347 <yellowjackets980@gmail.com>
2025-02-10 11:13:10 +0300

    a change

--- server.js
+++ server.js
@@ -335,6 +335,6 @@ app.all('*', (req, res, next) => {
 
 // Start Server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
 });
